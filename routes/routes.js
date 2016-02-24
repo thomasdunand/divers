4161a47f2fd9f575133b8dc53e387e14240038d9
Thomas Dunand <thomasdunand@luckycart.com>
2016-02-24 17:01:35 +0100

    restify

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+var _ = require('lodash');
+
+module.exports = function routes(app) {
+
+  app.get('hello', function(req, res, next){
+    res.send('world')
+  });
+
+
+};
+
---
+++ server.js
@@ -0,0 +1,38 @@
+/**
+ * Server start script
+ * @type {exports}
+ */
+var restify = require("restify");
+
+/**
+ * Server
+ * @type {Server}
+ */
+var server = restify.createServer({
+  name: 'totodudu'
+});
+
+server.use(restify.bodyParser({mapParams: true}));
+server.use(restify.authorizationParser());
+server.use(restify.queryParser());
+
+/**
+ * Events
+ */
+
+server.on('uncaughtException', function(request, response, route, err){
+  console.log(err.stack);
+  response.send(new restify.InternalServerError(err.message));
+});
+
+server.on('after', function(request, response, route, err){
+  if(err) console.log(err.stack);
+});
+
+server.listen(8080, function() {
+  console.log({evt:"system", msg: server.name +' is listening on ' + server.url});
+});
+
+require('./routes/email')(server);
+
+
